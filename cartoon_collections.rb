5c250e980a6e922e90975fa681d52a5f48537797
Tommy Lau <tlau425@yahoo.com>
2019-02-27 07:43:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ end
 
 def summon_captain_planet(array) # code an argument here
   # Your code here
-  
+  array.collect do |word|
+    word.capitalize << "!"
 end
 
 def long_planeteer_calls# code an argument here
