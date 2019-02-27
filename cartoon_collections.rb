d6a63603a369fae683765548fe50e1dbaabebf52
Tommy Lau <tlau425@yahoo.com>
2019-02-27 07:48:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,11 +12,14 @@ def summon_captain_planet(array) # code an argument here
   # Your code here
   array.collect do |word|
     word.capitalize << "!"
-end
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls_long)# code an argument here
   # Your code here
+  calls_long.any? do |word|
+    word.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
