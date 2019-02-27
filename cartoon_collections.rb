21e9c6d108339ab772ccf35ae0378f27ff4ffbd3
Tommy Lau <tlau425@yahoo.com>
2019-02-27 07:34:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   new_arr = []
-  new_arr.each_with_index do |dwarves|
-    puts "#{dwarves}"
+  new_arr.each_with_index do |dwarves, index|
+    puts "#{index+1}. #{dwarves}"
   end
 end
 
