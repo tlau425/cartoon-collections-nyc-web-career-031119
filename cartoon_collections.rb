d2da506972fc382ab24091dfc140d833ba2b3063
Tommy Lau <tlau425@yahoo.com>
2019-02-27 07:41:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,16 @@
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
-def roll_call_dwarves(array)# code an argument here
+def roll_call_dwarves(array) # code an argument here
   # Your code here
   array.each_with_index do |name, index|
     puts "#{index+1}. #{name}"
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array) # code an argument here
   # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
