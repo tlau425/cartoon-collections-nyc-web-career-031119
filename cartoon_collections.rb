65ddb3203a78f31afee4a017b338a3df58012c32
Tommy Lau <tlau425@yahoo.com>
2019-02-27 07:50:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,9 @@ def long_planeteer_calls(calls_long)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find do |word|
+    word == "cheddar"
 end
